56bdffe18eaa9086a5c59b8d52c5fdd02c6f1c12
jrildosilva <jrsrildo44@gmail.com>
2021-08-05 19:16:02 -0300

    curso javascript

---
+++ Aula 13/Ambiente de teste.js
@@ -0,0 +1,25 @@
+var c = 1
+do  {
+    console.log(`'tudo bem?' passo ${c} muito bem.`)
+    c++ 
+} while ( c <= 6)
+
+
+
+/*
+var c = 1
+while ( c <= 3) {
+    console.log(`'tudo bem?' passo ${c} muito bem.`)
+    c++ 
+}
+*/
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
---
+++ aula 12/ex013.js
@@ -0,0 +1,41 @@
+var agora = new Date
+var diaSem = agora.getDay()
+//diaSem = 8
+/*
+   0 = Domingo
+   1 = segunda
+   2 = terça
+   3 = quarta
+   4 = quinta
+   5 = sexta
+   6 = sábado
+*/ 
+console.log(diaSem) 
+// comando no Node.js através Node.js exec ( tecla F8) 
+// resposta: data/ hoje 05/08/21 quinta-feira = 4
+switch(diaSem) {
+    case 0:
+        console.log('domingo')
+        break
+    case 1:
+        console.log('segunda') 
+        break
+    case 2:
+        console.log('terça') 
+        break
+    case 3:
+        console.log('quarta') 
+        break
+    case 4:
+        console.log('quinta')  
+        break
+    case 5:
+        console.log('sexta') 
+        break
+    case 6:
+        console.log('sábado') 
+        break   
+    default:
+        console.log('[Erro]dia inválido')
+        break                  
+}
