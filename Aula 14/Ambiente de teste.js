82b8087671ab4a1a0e64f3b94922fa7df10f2ff6
jrildosilva <jrsrildo44@gmail.com>
2021-08-06 18:36:34 -0300

    aprendendo c extensão debugger chrome

---
+++ Aula 14/Ambiente de teste.js
@@ -0,0 +1,24 @@
+/*
+var c = 1 
+while ( c <= 10 ) {
+    console.log(c)
+    c++
+}
+
+
+var c = 1 
+do {
+    console.log(c)
+    c++
+}while ( c <= 5 )
+*/
+/*
+for( var c=1 ; c <= 12 ; c += 1 ) {
+    console.log( c)
+}
+*/
+console.log( 'vai começar...')
+for( var c=1 ; c <= 3 ; c++ ) {
+    console.log(c)
+}
+    console.log('Muito bom!')
