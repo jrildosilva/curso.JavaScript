f156300203e3ed42b983ee59eae49b815cbeb1be
jrildosilva <jrsrildo44@gmail.com>
2021-08-09 15:59:08 -0300

    aula 15, variáveis compostas

---
+++ Aula 15/Ambiente.js
@@ -0,0 +1,12 @@
+let num = [5,8,9,3]
+    
+    num.push(1)
+
+    
+    num.sort()
+console.log(`o vetor é  ${num}`)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`o meu primeiro vetor é ${num[0]}`)
+let pos = num.indexOf(4)
+console.log(` o valor 4 esta no posição ${pos}`)
+
